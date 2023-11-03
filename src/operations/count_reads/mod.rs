25802f89e2b01e6d29df547e6f76fbef0868ea86
Benjamin Schuster-Böckler <ben.sb@methodicate.com>
2023-11-03 23:10:06 +0000

    report rel position in alignment not position in read

--- src/operations/count_reads/mod.rs
+++ src/operations/count_reads/mod.rs
@@ -293,12 +293,12 @@ use super::FLAGS;
                             {
                                 // mod
                                 next_read_count.mod_count = next_read_count.mod_count+1;
-                                next_read_count.mod_cpgs.push(block[0] as usize);
+                                next_read_count.mod_cpgs.push((block[1].abs_diff(next_read_count.start as i64)) as usize);
                             }
                             else if read_base == b'C'
                             {
                                 //unmod
-                                next_read_count.unmod_cpgs.push(block[0] as usize);
+                                next_read_count.unmod_cpgs.push((block[1].abs_diff(next_read_count.start as i64)) as usize);
                             }
                             else 
                             {
