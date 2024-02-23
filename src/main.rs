b07ffb0badbe5dd27a058ae668e5f1f4a358c65a
Benjamin Schuster-Böckler <ben.sb@methodicate.com>
2024-02-23 21:01:01 +0000

    remove trailing whitespaces

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use rastair::sequence_segment::run_finder;
 
 #[derive(Parser)]
 #[command(author="Benjamin Schuster-Boeckler", version, about, long_about=None, arg_required_else_help = true)]
-struct Cli 
+struct Cli
 {
     #[arg(short, long, action=clap::ArgAction::Count)]
     verbosity: u8,
@@ -19,10 +19,10 @@ struct Cli
 
 
 #[derive(Subcommand)]
-enum Commands 
+enum Commands
 {
     /// Call methylation on a bam file
-    Call 
+    Call
     {
         /// A sorted and indexed bam file
         #[arg(value_name="BAM_FILE", value_parser=value_parser!(PathBuf))]
@@ -35,23 +35,23 @@ enum Commands
         /// Minimum mapping quality per aligned read [default: 1]
         #[arg(short='q', long)]
         min_mapq: Option<u8>,
-        
+
         /// Minimum base quality per base in a read [default: 10]
         #[arg(short='Q', long, value_parser = clap::value_parser!(u8).range(1..))]
         min_baseq: Option<u8>,
-        
+
         /// Limit depth at highly covered positions to improve performance [default: 500]
         #[arg(short='x', long, value_parser = clap::value_parser!(u32).range(1..))]
         max_depth: Option<u32>,
-        
+
         /// number of reference positions processed in-memory at once [default: 100000]
         #[arg(short='s', long, value_parser = clap::value_parser!(u32).range(1..))]
         chunk_size: Option<u32>,
-        
+
         /// Include reads that match all of these bit-flags (as decimal) [default: 3]
         #[arg(short='f', long)]
         required_flags: Option<u16>,
-        
+
         /// Exclude reads matching any of these bit-flags (as decimal) [default: 3852]
         #[arg(short='F', long)]
         excluded_flags: Option<u16>,
@@ -63,7 +63,7 @@ enum Commands
         /// Soft-trim the OT by that many bases from either end [default: 0,0,0,0]
         #[arg(long="nOT")]
         n_ot: Option<String>,
-        
+
         /// Soft-trim the OT by that many bases from either end [default: 0,0,0,0]
         #[arg(long="nOB")]
         n_ob: Option<String>,
@@ -71,7 +71,7 @@ enum Commands
         /// Number of threads to use inside htslib for decompression [default: 1]
         #[arg(long, value_parser = clap::value_parser!(u8).range(1..))]
         read_threads: Option<u8>,
-        
+
         /// Number of threads to use for variant calling
         #[arg(short='@', long, value_parser = clap::value_parser!(u8).range(1..))]
         threads: Option<u8>,
@@ -91,15 +91,15 @@ enum Commands
         /// Minimum mapping quality per aligned read [default: 1]
         #[arg(short='q', long)]
         min_mapq: Option<u8>,
-        
+
         /// number of reference positions processed in-memory at once [default: 100000]
         #[arg(short='s', long, value_parser = clap::value_parser!(u32).range(1..))]
         chunk_size: Option<u32>,
-        
+
         /// Include reads that match all of these bit-flags (as decimal) [default: 3]
         #[arg(short='f', long)]
         required_flags: Option<u16>,
-        
+
         /// Exclude reads matching any of these bit-flags (as decimal) [default: 3852]
         #[arg(short='F', long)]
         excluded_flags: Option<u16>,
@@ -115,7 +115,7 @@ enum Commands
     /// print a map of all CpGs in a fasta file
     MapCpgs
     {
-        /// A sorted and indexed bam file
+        /// An indexed fasta file
         #[arg(value_name="FASTA_FILE", value_parser=value_parser!(PathBuf))]
         fasta_file: PathBuf,
 
@@ -143,52 +143,52 @@ fn real_main() -> i32
         .verbosity(cli.verbosity as usize)
         .timestamp(stderrlog::Timestamp::Second)
         .init()
-        .unwrap();    
-    
+        .unwrap();
+
     match &cli.command
     {
-        Some(Commands::Call { 
-            bam_file, 
-            fasta_file, 
-            min_mapq, 
-            min_baseq, 
-            max_depth, 
-            chunk_size, 
-            required_flags, 
+        Some(Commands::Call {
+            bam_file,
+            fasta_file,
+            min_mapq,
+            min_baseq,
+            max_depth,
+            chunk_size,
+            required_flags,
             excluded_flags,
             exclude_ambiguous,
             n_ot,
             n_ob,
             read_threads,
-            threads }) => 
+            threads }) =>
             {
                 // TODO move the unboxing of Options to here
                 // instead of doing that inside run_caller
-                match count_variants::run_caller(bam_file, 
-                    fasta_file, 
-                    min_mapq, 
-                    min_baseq, 
-                    max_depth, 
-                    chunk_size, 
-                    required_flags, 
+                match count_variants::run_caller(bam_file,
+                    fasta_file,
+                    min_mapq,
+                    min_baseq,
+                    max_depth,
+                    chunk_size,
+                    required_flags,
                     excluded_flags,
                     exclude_ambiguous,
                     n_ot,
                     n_ob,
                     read_threads,
-                    threads ) 
+                    threads )
                 {
                     Ok(()) => 0,
-                    Err(e)  => 
+                    Err(e)  =>
                     {
                         error!("Error running caller: {}", e);
                         1
                     }
                 }
             },
-        Some(Commands::MapCpgs { 
-                fasta_file, 
-                chunk_size }) => 
+        Some(Commands::MapCpgs {
+                fasta_file,
+                chunk_size }) =>
                 {
                     let step_size = chunk_size.unwrap_or_default();
 
@@ -212,17 +212,17 @@ fn real_main() -> i32
                 read_threads }) =>
                 {
                     match count_reads::run_caller(
-                        bam_file, 
-                        fasta_file, 
-                        min_mapq, 
-                        chunk_size, 
-                        required_flags, 
+                        bam_file,
+                        fasta_file,
+                        min_mapq,
+                        chunk_size,
+                        required_flags,
                         excluded_flags,
                         all_reads,
-                        read_threads,) 
+                        read_threads,)
                     {
                         Ok(()) => 0,
-                        Err(e)  => 
+                        Err(e)  =>
                         {
                             error!("Error running caller: {}", e);
                             1
