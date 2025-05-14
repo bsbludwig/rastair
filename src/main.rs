dc0e4ec574b24bcdc348045d010ba78c9354465a
Pascal Hertleif <pascal@softleif.se>
2025-05-14 15:34:43 -0400

    remove unused code

--- src/main.rs
+++ src/main.rs
@@ -46,21 +46,3 @@ fn main() -> Result<()> {
 
     Ok(())
 }
-
-// bitflags! {
-//     #[derive(Debug, Clone, Copy)]
-//     struct Flags: u16 {
-//         const IS_PAIRED = 0x1;
-//         const IS_PROPERLY_PAIRED = 0x2;
-//         const IS_UNMAPPED = 0x4;
-//         const MATE_IS_UNMAPPED = 0x8;
-//         const IS_REVERSE_STRAND = 0x10;
-//         const MATE_IS_REVERSE_STRAND = 0x20;
-//         const IS_FIRST_IN_PAIR = 0x40;
-//         const IS_SECOND_IN_PAIR = 0x80;
-//         const IS_NOT_PRIMARY = 0x100;
-//         const IS_FAILED = 0x200;
-//         const IS_DUPLICATE = 0x400;
-//         const IS_SUPPLEMENTAL = 0x800;
-//     }
-// }
