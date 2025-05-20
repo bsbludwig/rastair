fbf7b70edce3c947c82d9b3bf2362e754928f16a
Pascal Hertleif <pascal@softleif.se>
2025-05-20 16:23:49 -0400

    Use RASTAIR_LOG for logging setup

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
 use clap::{CommandFactory as _, Parser as _};
-use color_eyre::eyre::Result;
+use color_eyre::{
+    Section,
+    eyre::{Context, Result},
+};
 use rastair2::call::{CallParams, call};
-use tracing::{debug, info};
-use tracing_subscriber::{fmt::format::FmtSpan, layer::SubscriberExt as _};
+use tracing::{debug, info, warn};
+use tracing_subscriber::{EnvFilter, fmt::format::FmtSpan, layer::SubscriberExt as _};
 
 #[derive(Debug, clap::Parser)]
 struct Cli {
     #[clap(subcommand)]
     command: Subcommand,
+
+    /// Enable more logging
+    #[arg(short, long, global = true)]
+    verbose: bool,
 }
 
 /// Rastair2
@@ -28,20 +35,43 @@ enum Subcommand {
 }
 
 fn main() -> Result<()> {
-    color_eyre::install()?;
-    let subscriber = tracing_subscriber::Registry::default()
-        .with(tracing_error::ErrorLayer::default())
-        .with(tracing_subscriber::EnvFilter::from_default_env())
-        .with(
-            tracing_subscriber::fmt::Layer::default()
-                .with_target(true)
-                .with_span_events(FmtSpan::CLOSE)
-                .with_writer(std::io::stderr),
-        )
-        .with(tracing_subscriber::EnvFilter::from_default_env());
-    tracing::subscriber::set_global_default(subscriber)?;
+    color_eyre::install()
+        .wrap_err("Failed to set up panic handler")
+        .note("Seeing this error message is somewhat ironic, we know")?;
 
     let args = Cli::parse();
+
+    let subscriber = {
+        let default_log_settings =
+            if args.verbose { "info,rastair2=debug" } else { "warn,rastair2=info" };
+        let mut env_filter = EnvFilter::new(default_log_settings);
+        if let Ok(env) = std::env::var("RASTAIR_LOG") {
+            for directive in env.split(',') {
+                if directive.is_empty() {
+                    continue;
+                }
+                match directive.parse() {
+                    Ok(parsed_directive) => {
+                        env_filter = env_filter.add_directive(parsed_directive);
+                    }
+                    Err(error) => {
+                        warn!(?error, "Warning: Invalid log directive `{directive}`");
+                    }
+                }
+            }
+        }
+
+        tracing_subscriber::Registry::default()
+            .with(tracing_error::ErrorLayer::default())
+            .with(env_filter)
+            .with(
+                tracing_subscriber::fmt::Layer::default()
+                    .with_target(true)
+                    .with_span_events(FmtSpan::CLOSE)
+                    .with_writer(std::io::stderr),
+            )
+    };
+    tracing::subscriber::set_global_default(subscriber)?;
 
     match args.command {
         Subcommand::Call(params) => {
