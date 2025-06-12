8234f80646a41d224e50debb31a2c66f5f12d63e
Pascal Hertleif <pascal@softleif.se>
2025-06-12 10:11:47 +0200

    Call: Move process into own model, make it impls

--- src/call.rs
+++ src/call.rs
@@ -1,27 +1,21 @@
 use crate::{
-    call::{methylation::params::MethylationCallingParams, variants::VariantCandidatePileup},
-    sequence::{ChunkRegion, Readers, Segment, SegmentsParams},
-    utils::{StrandFromRecord, TryAsBase as _},
-    vcf::{self, Filters},
+    call::methylation::params::MethylationCallingParams,
+    sequence::{ChunkRegion, SegmentsParams},
+    vcf::{self},
     vcf_writer,
 };
-use color_eyre::eyre::{Context, ContextCompat, Result};
+use color_eyre::eyre::{Result, WrapErr};
 use rastair2_vcf::Vcf;
-use rust_htslib::bam::{
-    FetchDefinition, Read as _,
-    pileup::{Alignment, Pileup},
-    record::Cigar,
-};
-use smallvec::SmallVec;
-use std::rc::Rc;
-use tracing::{Level, debug, info, instrument, trace, warn};
+use tracing::{debug, info, instrument, warn};
 
 mod methylation;
 mod metrics;
+mod process;
 mod variant_calling;
 pub mod variants;
 
-use variants::{PositionInRead, SeenBase, SeenBases};
+#[cfg(test)]
+pub mod test_helpers;
 
 #[derive(Debug, clap::Args)]
 pub struct CallParams {
@@ -58,10 +52,11 @@ pub fn call(params: &CallParams) -> Result<()> {
     // TODO: For multithreaded processing, have readers per thread, collect data in order, and write in main thread
     regions.into_iter().try_for_each(|region| {
         regions_seen += 1;
-        process_region(&region, &mut readers)
+        region
+            .process(&mut readers)
             .and_then(|piles| {
                 piles.into_iter().try_for_each(|pile| {
-                    variant_metrics(&pile)
+                    pile.variant_metrics()
                         .wrap_err("Failed to collect metrics")
                         .and_then(|record| {
                             params.methylation.call(record).wrap_err("Failed to call methylation")
@@ -77,162 +72,11 @@ pub fn call(params: &CallParams) -> Result<()> {
             })
             .wrap_err_with(|| format!("failed to process region {}", region.region))
     })?;
+
+    drop(vcf_writer); // Ensure all data is flushed to the output file
     info!("Wrote output to {}", params.vcf.vcf_output.display());
 
     return Ok(());
-}
-
-#[instrument(level = "info", skip_all, fields(region=%region.region))]
-fn process_region(
-    region: &ChunkRegion,
-    readers: &mut Readers,
-) -> Result<Vec<VariantCandidatePileup>> {
-    let segment = readers.segment(region).wrap_err("failed to fetch segment")?;
-    trace!(len = segment.sequence.len(), "Processing region");
-
-    // Fetch the pileups for the segment
-    FetchDefinition::try_from(&segment)
-        .wrap_err("Could not convert region string")
-        .and_then(|r| readers.bam.fetch(r).wrap_err("Could not fetch segment from BAM file"))
-        .wrap_err_with(|| format!("Could not fetch region `{}` from BAM file", region.region))?;
-
-    let segment = Rc::new(segment);
-    // Go over each column in the pileup and collect variant candidates
-    let piles = readers
-        .bam
-        .pileup()
-        .filter_map(|p| p.ok())
-        .filter(|p| {
-            // Filter out pileups that are not in the region of interest
-            region.contains(u64::from(p.pos()))
-        })
-        .flat_map(|pile| {
-            collect_candidate(&pile, segment.clone())
-                .wrap_err_with(|| {
-                    format!("Failed to get candidate from pileup at position {}", pile.pos())
-                })
-                .transpose()
-        })
-        .filter_map(|res| match res {
-            Ok(x) => Some(x),
-            Err(error) => {
-                warn!(%error, "Failed to get pileup, skipping");
-                None
-            }
-        })
-        .collect::<Vec<_>>();
-
-    if tracing::enabled!(Level::DEBUG) {
-        if piles.is_empty() {
-            trace!("No candidate piles found in region, skipping");
-            return Ok(piles);
-        } else {
-            let count = readable::num::Unsigned::from(piles.len());
-            let bytes = readable::byte::Byte::from(
-                piles.len() * std::mem::size_of::<VariantCandidatePileup>(),
-            );
-            debug!(%count, %bytes, "Collected candidates");
-        }
-    }
-
-    Ok(piles)
-}
-
-/// Is this pileup a candidate for a variant?
-#[instrument(level = "trace", skip_all)]
-fn collect_candidate(
-    pile: &Pileup,
-    segment: Rc<Segment>,
-) -> Result<Option<VariantCandidatePileup>> {
-    let segment_start_pos =
-        usize::try_from(segment.range.start).expect("segment range fits in usize");
-    let idx = usize::try_from(pile.pos())
-        .expect("position fits in usize")
-        .checked_sub(segment_start_pos)
-        .wrap_err_with(|| {
-            format!(
-                "pile position {} is not in segment range {}..{}",
-                pile.pos(),
-                segment.range.start,
-                segment.range.end
-            )
-        })?;
-    let bases = SeenBases(pile.alignments().filter_map(pileup_mapper).collect());
-    let reference_base =
-        segment.sequence.get(idx).wrap_err("failed to get reference base")?.as_base()?;
-
-    if bases.matches(reference_base) {
-        // Matches reference base, boring.
-        // trace!(?bases, pos = pile.pos(), ?reference_base, ?next_base, "pile matches reference");
-        Ok(None)
-    } else {
-        Ok(Some(VariantCandidatePileup {
-            segment: segment.clone(),
-            pos: pile.pos(),
-            bases,
-            reference_base,
-        }))
-    }
-}
-
-/// Collect info from a pileup alignment
-fn pileup_mapper(a: Alignment<'_>) -> Option<SeenBase> {
-    let pos = a.qpos()?;
-    let record = a.record();
-
-    // get number of matches from CIGAR
-    let matches: u32 =
-        record.cigar().iter().map(|c| if let Cigar::Match(n) = c { *n } else { 0 }).sum();
-    // get number of indels from CIGAR
-    let indels: u32 = record
-        .cigar()
-        .iter()
-        .map(|c| match c {
-            Cigar::Del(n) | Cigar::Ins(n) => *n,
-            _ => 0,
-        })
-        .sum();
-
-    if !record.is_proper_pair() {
-        // fixme: maybe be more lenient here
-        return None;
-    }
-    if record.is_quality_check_failed() {
-        return None;
-    }
-    // fixme: understand this better:
-    // if record.cigar().iter().any(|c| matches!(c, Cigar::SoftClip(_))) {
-    //     return None;
-    // }
-
-    Some(SeenBase {
-        qname: SmallVec::from(record.qname()),
-        base: record.seq()[pos].as_base().ok()?, // fixme: handle error or at least check usual error modes
-        qual: *record.qual().get(pos)?,
-        mapq: record.mapq(),
-        strand: StrandFromRecord::strand(&record).ok()?,
-        position: PositionInRead {
-            pos: u32::try_from(pos).expect("position fits in u32"),
-            read_length: u32::try_from(record.seq().len()).expect("read length fits in u32"),
-        },
-        matching_bases: matches,
-        indels,
-    })
-}
-
-/// Collect metrics
-#[instrument(level = "trace", skip_all)]
-fn variant_metrics(pile: &VariantCandidatePileup) -> Result<vcf::Record> {
-    let metrics = pile.metrics().wrap_err("Failed to calculate metrics")?;
-    let calling_metrics = pile.calling_metrics().wrap_err("Failed to calculate calling metrics")?;
-
-    Ok(vcf::Record {
-        fixed_fields: pile.fixed_fields(),
-        // FIXME: Add filters based on thresholds
-        filters: Filters::new().add(rastair2_vcf::standard_fields::PASS),
-        info: metrics,
-        samples: smallvec::smallvec![calling_metrics],
-    })
 }
 
 /// Write a pileup to the VCF output
---
+++ src/call/process.rs
@@ -0,0 +1,170 @@
+use crate::{
+    call::variants::{PositionInRead, SeenBase, SeenBases, VariantCandidatePileup},
+    sequence::{ChunkRegion, Readers, Segment},
+    utils::{StrandFromRecord, TryAsBase as _},
+    vcf::{self, Filters},
+};
+use color_eyre::eyre::{ContextCompat as _, Result, WrapErr};
+use rust_htslib::bam::{
+    FetchDefinition, Read as _,
+    pileup::{Alignment, Pileup},
+    record::Cigar,
+};
+use smallvec::SmallVec;
+use std::rc::Rc;
+use tracing::{Level, debug, instrument, trace, warn};
+
+impl ChunkRegion {
+    #[instrument(level = "info", skip_all, fields(region=%self.region))]
+    pub fn process(&self, readers: &mut Readers) -> Result<Vec<VariantCandidatePileup>> {
+        let segment = readers.segment(self).wrap_err("failed to fetch segment")?;
+        trace!(len = segment.sequence.len(), "Processing region");
+
+        // Fetch the pileups for the segment
+        FetchDefinition::try_from(&segment)
+            .wrap_err("Could not convert region string")
+            .and_then(|r| readers.bam.fetch(r).wrap_err("Could not fetch segment from BAM file"))
+            .wrap_err_with(|| format!("Could not fetch region `{}` from BAM file", self.region))?;
+
+        let segment = Rc::new(segment);
+        // Go over each column in the pileup and collect variant candidates
+        let piles = readers
+            .bam
+            .pileup()
+            .filter_map(|p| p.ok())
+            .filter(|p| {
+                // Filter out pileups that are not in the region of interest
+                self.contains(u64::from(p.pos()))
+            })
+            .flat_map(|pile| {
+                collect_candidate(&pile, segment.clone())
+                    .wrap_err_with(|| {
+                        format!("Failed to get candidate from pileup at position {}", pile.pos())
+                    })
+                    .transpose()
+            })
+            .filter_map(|res| match res {
+                Ok(x) => Some(x),
+                Err(error) => {
+                    warn!(%error, "Failed to get pileup, skipping");
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
+
+        if tracing::enabled!(Level::DEBUG) {
+            if piles.is_empty() {
+                trace!("No candidate piles found in region, skipping");
+                return Ok(piles);
+            } else {
+                let count = readable::num::Unsigned::from(piles.len());
+                let bytes = readable::byte::Byte::from(
+                    piles.len() * std::mem::size_of::<VariantCandidatePileup>(),
+                );
+                debug!(%count, %bytes, "Collected candidates");
+            }
+        }
+
+        Ok(piles)
+    }
+}
+
+/// Is this pileup a candidate for a variant?
+#[instrument(level = "trace", skip_all)]
+fn collect_candidate(
+    pile: &Pileup,
+    segment: Rc<Segment>,
+) -> Result<Option<VariantCandidatePileup>> {
+    let segment_start_pos =
+        usize::try_from(segment.range.start).expect("segment range fits in usize");
+    let idx = usize::try_from(pile.pos())
+        .expect("position fits in usize")
+        .checked_sub(segment_start_pos)
+        .wrap_err_with(|| {
+            format!(
+                "pile position {} is not in segment range {}..{}",
+                pile.pos(),
+                segment.range.start,
+                segment.range.end
+            )
+        })?;
+    let bases = SeenBases(pile.alignments().filter_map(pileup_mapper).collect());
+    let reference_base =
+        segment.sequence.get(idx).wrap_err("failed to get reference base")?.as_base()?;
+
+    if bases.matches(reference_base) {
+        // Matches reference base, boring.
+        // trace!(?bases, pos = pile.pos(), ?reference_base, ?next_base, "pile matches reference");
+        Ok(None)
+    } else {
+        Ok(Some(VariantCandidatePileup {
+            segment: segment.clone(),
+            pos: pile.pos(),
+            bases,
+            reference_base,
+        }))
+    }
+}
+
+/// Collect info from a pileup alignment
+fn pileup_mapper(a: Alignment<'_>) -> Option<SeenBase> {
+    let pos = a.qpos()?;
+    let record = a.record();
+
+    // get number of matches from CIGAR
+    let matches: u32 =
+        record.cigar().iter().map(|c| if let Cigar::Match(n) = c { *n } else { 0 }).sum();
+    // get number of indels from CIGAR
+    let indels: u32 = record
+        .cigar()
+        .iter()
+        .map(|c| match c {
+            Cigar::Del(n) | Cigar::Ins(n) => *n,
+            _ => 0,
+        })
+        .sum();
+
+    if !record.is_proper_pair() {
+        // fixme: maybe be more lenient here
+        return None;
+    }
+    if record.is_quality_check_failed() {
+        return None;
+    }
+    // fixme: understand this better:
+    // if record.cigar().iter().any(|c| matches!(c, Cigar::SoftClip(_))) {
+    //     return None;
+    // }
+
+    Some(SeenBase {
+        qname: SmallVec::from(record.qname()),
+        base: record.seq()[pos].as_base().ok()?, // fixme: handle error or at least check usual error modes
+        qual: *record.qual().get(pos)?,
+        mapq: record.mapq(),
+        strand: StrandFromRecord::strand(&record).ok()?,
+        position: PositionInRead {
+            pos: u32::try_from(pos).expect("position fits in u32"),
+            read_length: u32::try_from(record.seq().len()).expect("read length fits in u32"),
+        },
+        matching_bases: matches,
+        indels,
+    })
+}
+
+impl VariantCandidatePileup {
+    /// Collect metrics
+    #[instrument(level = "trace", skip_all)]
+    pub fn variant_metrics(&self) -> Result<vcf::Record> {
+        let metrics = self.metrics().wrap_err("Failed to calculate metrics")?;
+        let calling_metrics =
+            self.calling_metrics().wrap_err("Failed to calculate calling metrics")?;
+
+        Ok(vcf::Record {
+            fixed_fields: self.fixed_fields(),
+            // FIXME: Add filters based on thresholds
+            filters: Filters::new().add(rastair2_vcf::standard_fields::PASS),
+            info: metrics,
+            samples: smallvec::smallvec![calling_metrics],
+        })
+    }
+}
