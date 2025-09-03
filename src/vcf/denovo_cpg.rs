a0473d22a37543557035557f0991a5854db8ae87
Pascal Hertleif <pascal@softleif.se>
2025-09-03 19:51:50 +0200

    denovo: field docs

--- src/vcf/denovo_cpg.rs
+++ src/vcf/denovo_cpg.rs
@@ -7,9 +7,25 @@ use std::ops::Deref;
 /// De-novo CPG candidate: Could the alt alleles create a new CpG site?
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
 pub enum DeNovoCpGCandidate {
+    /// No, the variant cannot create a new CpG site
     NotCandidate,
-    Candidate { ref_base: Base, alt_base: Base, alt_index: usize },
-    Adjecent { ref_base: Base },
+    /// Yes, the variant could create a new CpG site
+    Candidate {
+        /// The reference base at the variant position
+        ref_base: Base,
+        /// The alternative base that creates a new CpG site
+        alt_base: Base,
+        /// The index of the alternative allele that creates a new CpG site
+        alt_index: usize,
+    },
+    /// Yes, the variant is adjacent to a de-novo CpG site
+    Adjecent {
+        /// The reference base at this adjacent position
+        ///
+        /// If the other position has a C variant, this is G; and if the other
+        /// position has a G variant, this is C.
+        ref_base: Base,
+    },
 }
 
 impl DeNovoCpGCandidate {
