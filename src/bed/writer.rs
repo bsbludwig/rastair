c7bcd4cb8013e69c1fb537cd99f70d78355da625
Pascal Hertleif <pascal@softleif.se>
2025-09-17 15:57:15 +0200

    bed: Write tabix files automatically
    fixes #29

--- src/bed/writer.rs
+++ src/bed/writer.rs
@@ -1,13 +1,25 @@
-use crate::bed::{BedFormat, BedRecord};
+use crate::{
+    bed::{BedFormat, BedRecord},
+    utils::logging::ThisIsABug,
+};
 use clio::ClioPath;
-use color_eyre::{Result, eyre::Context as _};
+use color_eyre::{
+    Result,
+    eyre::{Context as _, ContextCompat},
+};
+use noodles::{
+    bgzf::{self, io::writer::CompressionLevel},
+    core::Position,
+    csi::{self as csi, binning_index::index::reference_sequence::bin::Chunk},
+    tabix,
+};
 use std::{
     any::type_name,
     fmt,
     fs::File,
     io::{BufWriter, Write},
     marker::PhantomData,
-    path::Path,
+    path::PathBuf,
 };
 use tracing::{debug, info, instrument};
 
@@ -18,23 +30,34 @@ pub struct BedWriter<R: BedRecord> {
     record_type: PhantomData<R>,
 }
 
+type BoxedWriter = Box<dyn Write + Send + Sync>;
+
 enum Writer {
-    Bed(Box<dyn Write + Send + Sync>),
-    BedGz(bgzip::BGZFWriter<Box<dyn Write + Send + Sync>>),
+    /// BED as text files
+    Bed(BoxedWriter),
+    /// BGZF-compressed BED files, will write both tbi index and gzi index files
+    /// if the path is a local file
+    BedGz { bed: bgzf::io::Writer<BoxedWriter>, tabix: Option<TabixWriter> },
+}
+
+struct TabixWriter {
+    indexer: tabix::index::Indexer,
+    output_path: PathBuf,
+    output: tabix::io::Writer<BoxedWriter>,
 }
 
 impl Write for Writer {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         match self {
             Writer::Bed(writer) => writer.write(buf),
-            Writer::BedGz(writer) => writer.write(buf),
+            Writer::BedGz { bed: writer, .. } => writer.write(buf),
         }
     }
 
     fn flush(&mut self) -> std::io::Result<()> {
         match self {
             Writer::Bed(writer) => writer.flush(),
-            Writer::BedGz(writer) => writer.flush(),
+            Writer::BedGz { bed: writer, .. } => writer.flush(),
         }
     }
 }
@@ -46,15 +69,42 @@ impl<R: BedRecord> BedWriter<R> {
         let writer: Box<dyn Write + Send + Sync> = Box::new(BufWriter::new(writer));
         let mut writer: Writer = match format {
             BedFormat::BedGz => {
-                let writer = bgzip::BGZFWriter::with_compress_unit_size(
-                    writer,
-                    bgzip::Compression::fast(),
-                    bgzip::write::DEFAULT_COMPRESS_UNIT_SIZE,
-                    // Write index if the path is a local file
-                    path.is_local(),
-                )
-                .wrap_err("Failed to create BGZF writer")?;
-                Writer::BedGz(writer)
+                let writer = bgzf::io::writer::Builder::default()
+                    .set_compression_level(CompressionLevel::default())
+                    .build_from_writer(writer);
+                let tabix = if path.is_file() {
+                    let mut indexer = tabix::index::Indexer::default();
+                    indexer.set_header(csi::binning_index::index::header::Builder::bed().build());
+
+                    let output_path = path.with_file_name({
+                        let mut p = path
+                            .path()
+                            .file_name()
+                            .wrap_err_with(|| {
+                                format!(
+                                    "Failed to get file name from path `{}`",
+                                    path.path().display()
+                                )
+                            })?
+                            .to_os_string();
+                        p.push(".tbi");
+                        p
+                    });
+                    let output_writer: BoxedWriter = Box::new(BufWriter::new(
+                        File::create(&output_path).wrap_err_with(|| {
+                            format!(
+                                "Failed to create tabix index file for `{}`",
+                                path.path().display()
+                            )
+                        })?,
+                    ));
+                    let output = tabix::io::Writer::new(output_writer);
+                    debug!(path=?path.path(), "Writing tabix index");
+                    Some(TabixWriter { indexer, output_path, output })
+                } else {
+                    None
+                };
+                Writer::BedGz { bed: writer, tabix }
             }
             BedFormat::Bed => Writer::Bed(writer),
         };
@@ -64,33 +114,51 @@ impl<R: BedRecord> BedWriter<R> {
     }
 
     pub fn write_record(&mut self, record: &R) -> Result<()> {
-        record.write(&mut self.writer).wrap_err("Failed to write record")?;
-        writeln!(self.writer).wrap_err("Failed to write newline after record")?;
+        if let Writer::BedGz { bed, tabix } = &mut self.writer
+            && let Some(tabix) = tabix.as_mut()
+        {
+            let start_position = bed.virtual_position();
+
+            record.write(bed).wrap_err("Failed to write record")?;
+            writeln!(bed).wrap_err("Failed to write newline after record")?;
+
+            let end_position = bed.virtual_position();
+            let chunk = Chunk::new(start_position, end_position);
+
+            tabix.indexer.add_record(
+                record.chr(),
+                Position::new(record.start())
+                    .wrap_err("Failed to convert start to position")
+                    .this_is_a_bug()?,
+                Position::new(record.end())
+                    .wrap_err("Failed to convert end to position")
+                    .this_is_a_bug()?,
+                chunk,
+            )?;
+        } else {
+            record.write(&mut self.writer).wrap_err("Failed to write record")?;
+            writeln!(self.writer).wrap_err("Failed to write newline after record")?;
+        }
         Ok(())
     }
 
     /// Close the writer and write the index if applicable
     #[instrument(level = "debug", skip(self))]
     pub fn close(self) -> Result<()> {
-        // If BGZF, close and write index if we previously requested it.
-        //
-        // Intuitively, we would do this in a Drop impl, but since writing the
-        // index is quite explicit and can fail, we want to do this in a
-        // fallible method.
-        //
-        // We also wanted to first call `flush` here, but it seems that flushing
-        // a BGZF and then closing it cause it to double-write the last block in
-        // the version we tested with. Since closing also flushes, we just do
-        // that.
-        if let Writer::BedGz(bgzfwriter) = self.writer
-            && let Some(index) = bgzfwriter.close().wrap_err("Failed to close BGZF writer")?
-        {
-            write_index(self.path.path(), index)
-                .wrap_err_with(|| format!("Failed to write index for `{}`", self.path.display()))?;
-            info!(path = %self.path.display(), "Wrote bgzip index");
+        match self.writer {
+            Writer::Bed(mut writer) => writer.flush().wrap_err("Failed to flush BED writer")?,
+            Writer::BedGz { bed, tabix } => {
+                bed.finish().wrap_err("Failed to finish BGZF writer")?;
+                if let Some(mut tabix) = tabix {
+                    debug!("Writing tabix index");
+                    let index = tabix.indexer.build();
+                    tabix.output.write_index(&index).wrap_err_with(|| {
+                        format!("Failed to write tabix index to `{}`", tabix.output_path.display())
+                    })?;
+                    info!(path=?tabix.output_path, "Wrote tabix index");
+                }
+            }
         }
-        // For non-bgzip writers, self is dropped here, which also flushes the
-        // buffer.
         Ok(())
     }
 }
@@ -103,15 +171,4 @@ impl<R: BedRecord> fmt::Debug for BedWriter<R> {
             .field("format", &self.format)
             .finish()
     }
-}
-
-#[instrument(level = "debug")]
-fn write_index(original_path: &Path, index: bgzip::index::BGZFIndex) -> Result<()> {
-    let index_path = original_path.with_extension("gz.gzi");
-    let mut index_file = File::create(&index_path)
-        .wrap_err_with(|| format!("Failed to create index file `{}`", index_path.display()))?;
-    index
-        .write(&mut index_file)
-        .wrap_err_with(|| format!("Failed to write index to `{}`", index_path.display()))?;
-    Ok(())
 }
