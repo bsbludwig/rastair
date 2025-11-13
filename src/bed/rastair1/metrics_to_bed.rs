39ad041e6d7bf386a9b899cd7e108637064aeaea
Pascal Hertleif <pascal@softleif.se>
2025-11-13 10:29:15 +0100

    metrics2: cleaner bed conversion

--- src/bed/rastair1/metrics_to_bed.rs
+++ src/bed/rastair1/metrics_to_bed.rs
@@ -2,7 +2,7 @@ use crate::{
     bed::rastair1::{BedRecordsConvertParams, Rastair1BedFormat},
     call::variant_calling::GenotypeTag,
     metrics::PileupMetrics,
-    utils::{Base::*, ByStrand, logging::ThisIsABug as _},
+    utils::{Base::*, logging::ThisIsABug as _},
     vcf::{GenotypeConfidence, GenotypeLikelihood},
 };
 use color_eyre::{
@@ -28,73 +28,67 @@ impl Rastair1BedFormat {
             return Ok(None);
         }
 
+        let ref_base = pileup.ref_base();
+
         // If a position is covered by both a ref CpG site and a de-novo CpG
         // site, the ref case should take precedence.
-        let de_novo = !*pileup.pos_metrics.cpg && pileup.forms_denovo();
+        let cpg = *pileup.pos_metrics.cpg;
+        let de_novo = !cpg && pileup.forms_denovo();
 
         let Some(gt) = pileup.pos_metrics.genotype else {
-            warn!("No genotype for record");
+            debug!("No genotype for record");
             return Ok(None);
         };
 
-        #[derive(Debug, Default)]
-        struct Counts {
-            unmod: u32,
-            r#mod: u32,
-            no_snp: u32,
-            snp: u32,
-        }
-
-        impl Counts {
-            fn new(r: ByStrand<u32>, alt: ByStrand<u32>) -> Self {
-                Counts { unmod: r.ot, r#mod: alt.ot, no_snp: r.ob, snp: alt.ob }
-            }
-
-            fn total(&self) -> u32 {
-                self.unmod + self.r#mod + self.no_snp + self.snp
-            }
-        }
-
-        let counts = if pileup.ref_base() == C {
+        let counts = if cpg && ref_base == C {
             let r = pileup.ref_metrics.strand_count;
             let alt = pileup.alt(T).map(|a| a.strand_count).unwrap_or_default();
-
-            Counts::new(r, alt)
-        } else if pileup.ref_base() == G {
+            Counts { unmod: r.ot, r#mod: alt.ot, no_snp: r.ob, snp: alt.ob }
+        } else if cpg && ref_base == G {
             let r = pileup.ref_metrics.strand_count;
             let alt = pileup.alt(A).map(|a| a.strand_count).unwrap_or_default();
-
-            Counts::new(r, alt)
+            Counts { unmod: r.ob, r#mod: alt.ob, no_snp: r.ot, snp: alt.ot }
         } else {
-            // Counts::default()
-            todo!("Writing BED but ref is neither C nor G, so this is a de-novo candidate?")
+            // TOOD: Writing BED but ref is neither C nor G, so this is a de-novo candidate? Handle it!
+            Counts::default()
         };
 
         // If this looks like SNP, set beta to 0
-        // - Does ML say this is a true variant?
         let beta = if let Some(alt_base) = pileup.pos_metrics.cpg.alt_base()
             && let Some(alt) = pileup.alt_filters(alt_base)
             && let Some(score) = alt.ml
-            && let threshold = params.ml_threshold
-            && score >= threshold
         {
-            Some(Probability::new_panicky(0.0))
+            let threshold = params.ml_threshold;
+            if score >= threshold {
+                // - Does ML say this is a true variant?
+                Some(Probability::new_panicky(0.0))
+            } else if let Some(beta) = pileup.pos_metrics.methylated.beta() {
+                // - ML says not a variant, use beta if available
+                Some(Probability::new(beta).wrap_err("Beta value out of range").this_is_a_bug()?)
+            } else {
+                // - ML says not a variant, but no beta available
+                debug!(%score, %threshold, "ML says not a variant, but no beta available");
+                Some(Probability::new_panicky(0.0))
+            }
         } else if *pileup.pos_metrics.cpg
             && let Some(gt) = pileup.pos_metrics.genotype
             && gt.genotype == GenotypeTag::CT
         {
+            // - Is it in a CpG and called as heterozygous?
             Some(Probability::new_panicky(0.0))
         } else if let Some(beta) = pileup.pos_metrics.methylated.beta() {
+            // - Just use beta if available
             Some(Probability::new(beta).wrap_err("Beta value out of range").this_is_a_bug()?)
         } else {
-            warn!(in_cpg=?pileup.pos_metrics.cpg, genotype=?pileup.pos_metrics.genotype, "why no beta?");
+            // - No beta available
+            warn!(in_cpg=?pileup.pos_metrics.cpg, ?de_novo, genotype=?pileup.pos_metrics.genotype, "why no beta?");
             None
         };
 
         let bed = super::Rastair1BedFormat {
             contig: pileup.contig(),
             pos: pileup.pos() as usize,
-            r#ref: pileup.ref_base().into(),
+            r#ref: ref_base.into(),
             beta,
             unmod: counts.unmod,
             r#mod: counts.r#mod,
@@ -122,5 +116,19 @@ impl Rastair1BedFormat {
         }
 
         Ok(Some(bed))
+    }
+}
+
+#[derive(Debug, Default)]
+struct Counts {
+    unmod: u32,
+    r#mod: u32,
+    no_snp: u32,
+    snp: u32,
+}
+
+impl Counts {
+    fn total(&self) -> u32 {
+        self.unmod + self.r#mod + self.no_snp + self.snp
     }
 }
