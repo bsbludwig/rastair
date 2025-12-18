45c8454e84b5c9810563ff8b9ead711a9ea30655
Pascal Hertleif <pascal@softleif.se>
2025-12-18 11:20:48 +0100

    convert: speed up convert tests

--- tests/convert_cli.rs
+++ tests/convert_cli.rs
@@ -2,7 +2,7 @@ mod utils;
 use utils::*;
 
 #[test]
-fn write_mpk_then_convert_to_bcf() -> Result<()> {
+fn convert_from_mpk() -> Result<()> {
     apply_common_filters!();
 
     let temp_dir = TempDir::new()?;
@@ -40,30 +40,6 @@ fn write_mpk_then_convert_to_bcf() -> Result<()> {
         .succeeds()
         .wrap_err("Failed to convert to vcf.gz")?;
 
-    Ok(())
-}
-
-#[test]
-fn write_mpk_then_convert_to_bed() -> Result<()> {
-    apply_common_filters!();
-
-    let temp_dir = TempDir::new()?;
-    let mpk = temp_dir.path().join("test.mpk.lz4");
-
-    rastair()
-        .args([
-            "call",
-            "--fasta-file=tests/data/test.fasta.gz",
-            "tests/data/test.bam",
-            "--no-ml", // disable ML for faster test
-            "--region=chr19:6105700-6105800",
-            "-o",
-        ])
-        .arg(&mpk)
-        .silent()
-        .succeeds()
-        .wrap_err("Failed to run rastair call")?;
-
     rastair()
         .args(["convert", "--input"])
         .arg(&mpk)
@@ -77,109 +53,37 @@ fn write_mpk_then_convert_to_bed() -> Result<()> {
 }
 
 #[test]
-fn write_bcf_then_convert_to_bed() -> Result<()> {
+fn convert_vcf_to_same_bed() -> Result<()> {
     apply_common_filters!();
 
     let temp_dir = TempDir::new()?;
-    let bcf = temp_dir.path().join("test.bcf");
-
-    rastair()
-        .args([
-            "call",
-            "--fasta-file=tests/data/test.fasta.gz",
-            "tests/data/test.bam",
-            "--no-ml", // disable ML for faster test
-            "--region=chr19:6105700-6105800",
-            "-o",
-        ])
-        .arg(&bcf)
-        .silent()
-        .succeeds()
-        .wrap_err("Failed to run rastair call")?;
-
-    rastair()
-        .args(["convert", "--input"])
-        .arg(&bcf)
-        .arg("--output")
-        .arg(temp_dir.path().join("test.bed"))
-        .succeeds()
-        .wrap_err("Failed to convert to bed")?;
-
-    // use clio::ClioPath;
-    // use rastair_types::Probability;
-    // rastair::convert(&rastair::ConvertParams {
-    //     input: ClioPath::new(&bcf)?,
-    //     input_format: None,
-    //     output: ClioPath::new(temp_dir.path().join("test.bed"))?,
-    //     output_format: None,
-    //     bed_params: Default::default(),
-    //     ml_threshold: Probability::new_panicky(0.8),
-    // })?;
-
-    Ok(())
-}
-
-#[test]
-fn same_bed_via_mkv_and_bcf_conversion() -> Result<()> {
-    apply_common_filters!();
-
-    let temp_dir = TempDir::new()?;
-    let mpk = temp_dir.path().join("test.mpk.lz4");
     let vcf = temp_dir.path().join("test.vcf");
     let bed = temp_dir.path().join("test.bed");
-    let bed_from_mpk = temp_dir.path().join("from_mpk.bed");
     let bed_from_vcf = temp_dir.path().join("from_vcf.bed");
 
-    rastair()
-        .args([
-            "call",
-            "--fasta-file=tests/data/test.fasta.gz",
-            "tests/data/test.bam",
-            "--no-ml", // disable ML for faster test
-            "--region=chr19:6105700-6105800",
-            "-o",
-        ])
-        .arg(&mpk)
-        .silent()
-        .succeeds()
-        .wrap_err("Failed to run rastair call to mpk")?;
+    let args = &[
+        "call",
+        "--fasta-file=tests/data/test.fasta.gz",
+        "tests/data/test.bam",
+        "--region=chr19:6105700-6105800",
+    ];
 
     rastair()
-        .args([
-            "call",
-            "--fasta-file=tests/data/test.fasta.gz",
-            "tests/data/test.bam",
-            "--no-ml", // disable ML for faster test
-            "--region=chr19:6105700-6105800",
-            "-o",
-        ])
+        .args(args)
+        .arg("-o")
         .arg(&vcf)
         .silent()
         .succeeds()
         .wrap_err("Failed to run rastair call to vcf")?;
 
     rastair()
-        .args([
-            "call",
-            "--fasta-file=tests/data/test.fasta.gz",
-            "tests/data/test.bam",
-            "--no-ml", // disable ML for faster test
-            "--region=chr19:6105700-6105800",
-            "--bed",
-        ])
+        .args(args)
+        .arg("-c")
+        .arg("--bed")
         .arg(&bed)
         .silent()
         .succeeds()
-        .wrap_err("Failed to run rastair call to bed")?;
-
-    rastair()
-        .args(["convert", "--input"])
-        .arg(&mpk)
-        .arg("--output")
-        .arg(&bed_from_mpk)
-        .silent()
-        .succeeds()
-        .wrap_err("Failed to convert mpk to bed")?;
+        .wrap_err("Failed to run rastair call to vcf")?;
 
     rastair()
         .args(["convert", "--input"])
@@ -196,9 +100,6 @@ fn same_bed_via_mkv_and_bcf_conversion() -> Result<()> {
     let bed_vcf = std::fs::read_to_string(&bed_from_vcf)?;
     assert_eq!(bed_directly, bed_vcf, "BED files from VCF conversions do not match");
 
-    let bed_mpk = std::fs::read_to_string(&bed_from_mpk)?;
-    assert_eq!(bed_directly, bed_mpk, "BED files from MPK conversions do not match");
-
     Ok(())
 }
 
@@ -209,7 +110,7 @@ fn can_pipe_through() -> Result<()> {
     let mut cmd = Command::new(insta_cmd::get_cargo_bin("/bin/bash"));
     cmd.arg("-c");
     cmd.env("NO_COLOR", "1");
-    cmd.arg("cargo run -q --release -- call --fasta-file=tests/data/test.fasta.gz tests/data/test.bam --no-ml --vcf | head -n1000 | cargo run -q --release -- convert -f bcf -F bed | head -n5");
+    cmd.arg("cargo run -q -- call --fasta-file=tests/data/test.fasta.gz tests/data/test.bam --region=chr19:6105000-6105800 --no-ml --vcf | head -n1000 | cargo run -q -- convert -f bcf -F bed | head -n5");
 
     assert_cmd_snapshot!(cmd);
 
