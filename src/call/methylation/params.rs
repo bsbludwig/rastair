2414c182cabe729aa90952eaef766acbc1e247f2
Pascal Hertleif <pascal@softleif.se>
2026-08-21 14:20:41 +0200

    docs: Add more CLI arg sections

--- src/call/methylation/params.rs
+++ src/call/methylation/params.rs
@@ -11,37 +11,37 @@ pub struct MethylationCallingParams {
 pub struct ThresholdParams {
     /// The minimum variant allele frequency
     #[arg(long, default_value_t = 0.2)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(0.2)]
     pub m_vaf_min: f64,
 
     /// The minimum number of reads to call a position as methylated
     #[arg(long, default_value_t = 3)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(3)]
     pub m_min_depth: u32,
 
     /// The minimum quality ratio `(ad_alt*bq_alt + 1) / (ad_ref*bq_ref + 1)`
     #[arg(long, default_value_t = 0.27)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(0.27)]
     pub m_bq_ratio_min: f64,
 
     /// The minimum relative position in read for alt allele evidence
     #[arg(long, default_value_t = 0.2)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(0.2)]
     pub m_read_position_min: f64,
 
     /// The maximum relative position in read for alt allele evidence
     #[arg(long, default_value_t = 0.8)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(0.8)]
     pub m_read_position_max: f64,
 
     /// The maximum coverage depth for methylation calling
     #[arg(long, default_value_t = 1000)]
-    #[arg(help_heading = cli::sections::FILTER)]
+    #[arg(help_heading = cli::sections::METHYLATION)]
     #[default(1000)]
     pub m_max_coverage: u32,
 }
